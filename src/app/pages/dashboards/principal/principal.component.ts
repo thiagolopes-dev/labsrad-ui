fa31c02db4862744efd725b3d1428bffed1a0b6b
Thiago Lopes <thiagoolopezz@gmail.com>
2023-11-30 14:31:11 -0300

    Dashboard integrado ao Projeto

--- src/app/pages/dashboards/principal/principal.component.ts
+++ src/app/pages/dashboards/principal/principal.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./principal.component.css'],
 })
 export class PrincipalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  urlMetabase: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) {
+    if (window.location.hostname === 'app.labsrad.com.br') {
+      this.urlMetabase = this.sanitizer.bypassSecurityTrustResourceUrl(
+        `https://app.labsrad.com.br:3000/public/dashboard/b078ded4-9052-4091-952c-1f9d3cb4fefc`,
+      );
+    } else if (window.location.hostname === 'app-hml.connectvarejo.com.br') {
+      this.urlMetabase = this.sanitizer.bypassSecurityTrustResourceUrl(
+        `https://app-hml.labsrad.com.br:3000/public/dashboard/b078ded4-9052-4091-952c-1f9d3cb4fefc`,
+      );
+    } else {
+      this.urlMetabase = this.sanitizer.bypassSecurityTrustResourceUrl(
+        `http://localhost:3000/public/dashboard/b078ded4-9052-4091-952c-1f9d3cb4fefc`,
+      );
+    }
   }
 
+  ngOnInit() {}
 }
